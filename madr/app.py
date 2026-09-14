ab4c485b447dcec485e428b6ff397f0091d30a28
CaioWill <caio5.william5@gmail.com>
2026-09-14 18:01:24 -0300

    feat: criando a aplicação

---
+++ madr/app.py
@@ -0,0 +1,19 @@
+from http import HTTPStatus
+
+from fastapi import FastAPI
+
+from madr.routes import auth, contas
+from madr.schemas.schema import Mensagem
+
+app = FastAPI()
+
+app.include_router(contas.router)
+app.include_router(auth.router)
+# app.include_router(livros.router)
+# app.include_router(autores.router)
+# app.include_router(emprestimos.router)
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Mensagem)
+def pagina_inicial():
+    return {'mensagem': 'olá'}
