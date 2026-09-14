893af3bb74676daae8ee4e2a8e22fc0dfc5ce20c
CaioWill <caio5.william5@gmail.com>
2026-09-14 18:09:29 -0300

    feat: Criação do arquivo de schemas do routes de contas e auth

---
+++ madr/schemas/schema_Auths.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+from pydantic import BaseModel, EmailStr
+
+
+class Credenciais(Enum):
+    adimin = 'Admin'
+    user = 'User'
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+    credenciais: Credenciais
+
+
+class UpdateCredencias(BaseModel):
+    username: str
+    credenciais: Credenciais
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
