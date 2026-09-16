4a884ca4aedafabba9289855085f2733c33826a6
CaioWill <caio5.william5@gmail.com>
2026-09-16 17:37:46 -0300

    feat: adicionando o novo router admin a aplicação

--- madr/app.py
+++ madr/app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from madr.routes import auth, contas
+from madr.routes import admin, auth, contas
 from madr.schemas.schema import Mensagem
 
 app = FastAPI()
 
 app.include_router(contas.router)
 app.include_router(auth.router)
+app.include_router(admin.router)
 # app.include_router(livros.router)
 # app.include_router(autores.router)
 # app.include_router(emprestimos.router)
