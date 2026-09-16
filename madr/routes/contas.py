74785eb3003a104d3fcb0d63e5ad8914c5fc7af5
CaioWill <caio5.william5@gmail.com>
2026-09-16 17:09:35 -0300

    feat: Adicionando a funcionalidade de criar contas admin e de alteração acesso de contas diferentes para admin com contas que já são admin

--- madr/routes/contas.py
+++ madr/routes/contas.py
@@ -11,12 +11,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from madr.database_conect import get_session
 from madr.models import User
 from madr.schemas.schema_Auths import (
-    UpdateCredencias,
+    AdminPublic,
+    UpdateAdmin,
+    UserList,
     UserPublic,
     UserSchema,
     UserUpdate,
 )
 from madr.security import criptografar, get_current
+from madr.settings import Settings
 
 router = APIRouter(prefix='/login', tags=['login'])
 
@@ -56,17 +59,44 @@ async def criar_conta(session: Session, user: UserSchema):
                 status_code=HTTPStatus.CONFLICT, detail='Email já existente'
             )
 
+    is_admin = False
+
+    if user.is_admin != 'string':
+        if user.is_admin == Settings().ADMIN_KEY:
+            is_admin = True
+        else:
+            raise HTTPException(
+                status_code=HTTPStatus.FORBIDDEN,
+                detail='Senha de autorização incorreta',
+            )
+
     # adicionando o novo user no db
     response = User(
         username=user.username,
         email=user.email,
         password=criptografar(user.password),
+        is_admin=is_admin,
     )
 
     session.add(response)
     await session.commit()
     await session.refresh(response)
     return response
+
+
+@router.get(
+    '/listar_usuarios', status_code=HTTPStatus.OK, response_model=UserList
+)
+async def usuarios(current_user: Current_user, session: Session):
+    if current_user.is_admin:
+        users = await session.scalars(select(User))
+    else:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN,
+            detail='Você não tem autorização para ver os usuarios',
+        )
+
+    return {'users': users}
 
 
 # Endpoint para atualizar um novo usuario
@@ -106,37 +136,44 @@ async def update_user(
 @router.put(
     '/credenciais',  # adicionamos a variavel, paramentro da url
     status_code=HTTPStatus.OK,
-    response_model=UserPublic,
+    response_model=AdminPublic,
 )
 async def update_credenciais_users(
-    user: UpdateCredencias,
+    user: UpdateAdmin,
     session: Session,
     current_user: Current_user,
 ):
 
-    if current_user.credenciais != 'Admin':
+    if not current_user.is_admin:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
 
-    user_update = await session.scalar(
+    response = await session.scalar(
         select(User).where(User.username == user.username)
     )
 
-    if not user_update:
+    if not response:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='Usuario não encontrado'
         )
 
-    user_update.credenciais = user.credenciais
+    if user.credencial == 'admin':
+        response.is_admin = True
+    elif user.credencial == 'user':
+        response.is_admin = False
+    else:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='Digite um valor valido'
+        )
 
     # tentando fazer o commit da trasação
     try:
-        session.add(user_update)
+        session.add(response)
         await session.commit()
-        await session.refresh(user_update)
+        await session.refresh(response)
 
-        return current_user
+        return response
 
     # Caso dê erro de integridade
     except IntegrityError:
