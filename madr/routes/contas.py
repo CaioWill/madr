9a2fbbd0721304b8cd5aeaaa1998f3083ccc1d91
CaioWill <caio5.william5@gmail.com>
2026-09-16 19:06:58 -0300

    feat: Adicionando o endpoind de deletar a conta logada. removendo endpoints e migrando para o router admin

--- madr/routes/contas.py
+++ madr/routes/contas.py
@@ -10,16 +10,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from madr.database_conect import get_session
 from madr.models import User
+from madr.schemas.schema import Mensagem
 from madr.schemas.schema_Auths import (
-    AdminPublic,
-    UpdateAdmin,
-    UserList,
     UserPublic,
     UserSchema,
     UserUpdate,
 )
 from madr.security import criptografar, get_current
-from madr.settings import Settings
 
 router = APIRouter(prefix='/login', tags=['login'])
 
@@ -59,44 +56,17 @@ async def criar_conta(session: Session, user: UserSchema):
                 status_code=HTTPStatus.CONFLICT, detail='Email já existente'
             )
 
-    is_admin = False
-
-    if user.is_admin != 'string':
-        if user.is_admin == Settings().ADMIN_KEY:
-            is_admin = True
-        else:
-            raise HTTPException(
-                status_code=HTTPStatus.FORBIDDEN,
-                detail='Senha de autorização incorreta',
-            )
-
     # adicionando o novo user no db
     response = User(
         username=user.username,
         email=user.email,
         password=criptografar(user.password),
-        is_admin=is_admin,
     )
 
     session.add(response)
     await session.commit()
     await session.refresh(response)
     return response
-
-
-@router.get(
-    '/listar_usuarios', status_code=HTTPStatus.OK, response_model=UserList
-)
-async def usuarios(current_user: Current_user, session: Session):
-    if current_user.is_admin:
-        users = await session.scalars(select(User))
-    else:
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN,
-            detail='Você não tem autorização para ver os usuarios',
-        )
-
-    return {'users': users}
 
 
 # Endpoint para atualizar um novo usuario
@@ -132,53 +102,12 @@ async def update_user(
         )
 
 
-# Endpoint para adm atualizar credenciais dos outros
-@router.put(
-    '/credenciais',  # adicionamos a variavel, paramentro da url
-    status_code=HTTPStatus.OK,
-    response_model=AdminPublic,
-)
-async def update_credenciais_users(
-    user: UpdateAdmin,
-    session: Session,
-    current_user: Current_user,
-):
+@router.delete('/delete', status_code=HTTPStatus.OK, response_model=Mensagem)
+async def delete_user(current_user: Current_user, session: Session):
 
-    if not current_user.is_admin:
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
-        )
+    name = current_user.username
 
-    response = await session.scalar(
-        select(User).where(User.username == user.username)
-    )
+    await session.delete(current_user)
+    await session.commit()
 
-    if not response:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='Usuario não encontrado'
-        )
-
-    if user.credencial == 'admin':
-        response.is_admin = True
-    elif user.credencial == 'user':
-        response.is_admin = False
-    else:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail='Digite um valor valido'
-        )
-
-    # tentando fazer o commit da trasação
-    try:
-        session.add(response)
-        await session.commit()
-        await session.refresh(response)
-
-        return response
-
-    # Caso dê erro de integridade
-    except IntegrityError:
-        await session.rollback()
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='User name or Email already exists',
-        )
+    return {'mensagem': f'Conta com username: {name} deletada permanetimente!'}
