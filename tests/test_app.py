4cf9332d0df33e2fe49ba3919269e6d20f07916a
CaioWill <caio5.william5@gmail.com>
2026-09-17 21:57:20 -0300

    test: alterando a fixture do test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 
-def test_app_tela_inicial(cliente):
-    response = cliente.get('/')
+def test_app_tela_inicial(client):
+    response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
