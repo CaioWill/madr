fbf27147cee079e3030e759cf7b1e6748562bb9a
CaioWill <caio5.william5@gmail.com>
2026-09-18 16:24:49 -0300

    feat: craindo tabelas de autores e livros

--- madr/models.py
+++ madr/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import false, func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, false, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 tabelas = registry()
 
@@ -21,3 +21,36 @@ class User:
     atualizacao: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), server_onupdate=func.now()
     )
+
+
+@tabelas.mapped_as_dataclass
+class Romancistas:
+    __tablename__ = 'romancistas'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    update_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), server_onupdate=func.now()
+    )
+    livros: Mapped[list['Livros']] = relationship(
+        init=False, cascade='all, delete-orphan', lazy='selectin'
+    )
+
+
+@tabelas.mapped_as_dataclass
+class Livros:
+    __tablename__ = 'livros'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str]
+    create: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    update_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), server_onupdate=func.now()
+    )
+    author: Mapped[int] = mapped_column(ForeignKey(Romancistas.id))
