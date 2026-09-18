986fcca3bcf89587c5c12f08efae94c88b1c3e87
CaioWill <caio5.william5@gmail.com>
2026-09-17 21:49:09 -0300

    chore: Adicionando um comentario para que a conecxao com o db n passe nos tests

--- madr/database_conect.py
+++ madr/database_conect.py
@@ -5,6 +5,6 @@ from madr.settings import Settings
 engine = create_async_engine(Settings().DATABASE_URL)
 
 
-async def get_session():
+async def get_session():  # pragma: no cover
     async with AsyncSession(engine, expire_on_commit=False) as Session:
         yield Session
